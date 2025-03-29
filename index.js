508ab290162dbc5f998e55efaaea316b6f8fee9f
Sthefanny Jaiane Silveira Santos <s.jaianecel@gmail.com>
2025-03-29 16:31:32 -0300

    rotas estruturadas'
    conexão com banco feita, nenhuma autentificarão ainda

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
     const express = require('express');
     const DatabaseClient = require('./utils/database')
     const userRoutes = require("./routes/user");
+    const eventRoutes = require("./routes/events");
+    const actionRoutes = require("./routes/actions");
     const dotenv = require('dotenv');
 
     dotenv.config();
 
     const app = express();
 
+    app.use(express.static('public'));
+
     app.use(express.json());
 
     app.use('/user', userRoutes); 
+    app.use('/events', eventRoutes); 
+    app.use('/action', actionRoutes); 
 
     app.listen(process.env.PORT, () => {
         console.log('Server rodando porta 3000')
---
+++ routes/actions.js
@@ -0,0 +1,57 @@
+const DatabaseClient = require('../utils/database');
+const express = require('express');
+
+const db = new DatabaseClient();
+
+const router = express.Router();
+
+router.post('/subscribeEvent', async (req, res) => {
+    const { userId, eventId } = req.body;
+
+    try{
+        const sql = 'INSERT INTO tb_inscricoes (usuario_id, evento_id) VALUES (?,?)';
+
+        const result = await db.executar(sql, [userId, eventId]);
+
+        res.status(201).json({ message: 'Usuário inscrito com sucesso'});
+    } catch (err) {
+        res.status(500).json({message: 'Não foi possível se inscrever no evento'})
+    }
+})
+
+router.delete('/cancelSubscription/:user/:event', async (req, res) => {
+    const { user, event } = req.params;
+
+    try {
+        const sql = 'DELETE FROM tb_inscricoes WHERE usuario_id = ? AND evento_id = ?';
+
+        const result = await db.executar(sql, [user, event]);
+        
+        res.status(200).json({ message: 'Inscrição cancelada com sucesso'})
+
+    } catch (err) {
+        res.status(500).json({ message: 'Não foi possível cancelar a inscrição'})
+
+    }
+})
+
+router.get('/userEventsSubscribed/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const sql = ' SELECT e.* FROM tb_inscricoes i JOIN tb_eventos e ON i.evento_id = e.id WHERE i.usuario_id = ?';
+
+        const result = await db.executar(sql, [id]);
+
+        if(result.length == 0){
+            return res.status(404).json({ message: 'Não foi possível ou não há eventos inscritos pelo usuário'});
+        }
+
+        res.status(200).json(result);
+
+    } catch (err) {
+        res.status(404).json({ message: 'Não foi possível acessar os eventos'});
+    }
+})
+
+module.exports = router;
