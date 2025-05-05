ae0243a63f11cef89726adbdb4256a64777c7f8a
Sthefanny Jaiane Silveira Santos <s.jaianecel@gmail.com>
2025-05-05 20:57:19 -0300

    Menu logo fixed and Loader

--- public/js/menu.js
+++ public/js/menu.js
@@ -6,12 +6,15 @@ function menu() {
     container.innerHTML =
         `
         <div class="container-fluid px-3">
+            <a class="navbar-brand fw-medium text-dark fw-bolder" href="/">
+                <span style="color: #ab38d8; font-size: 1.55rem;">+</span>Cultura
+            </a>
+
             <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-controls="navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
     
             <div class="collapse navbar-collapse d-lg-flex" id="navbarsExample11">
-                <a class="navbar-brand col-lg-3 me-0 fw-medium text-dark fw-bolder" href="/"><span style="color: #ab38d8; font-size: 1.55rem;">+</span>Cultura</a>
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item ">
                         <a class="nav-link text-body-tertiary menu-links" href="/eventsAvaliable">Eventos Disponíveis</a>
