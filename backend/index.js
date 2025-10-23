869b804026a70843e9eb069415f381ada46538e8
Sthefanny Jaiane Silveira Santos <s.jaianecel@gmail.com>
2025-10-23 19:55:03 -0300

    http test

--- backend/index.js
+++ backend/index.js
@@ -6,19 +6,12 @@
     const dotenv = require('dotenv');
     const cors = require('cors');
     const path = require('path');
-    const https = require('https');
 
     dotenv.config();
 
     const app = express();
 
-    const corsOptions = {
-        origin: 'http://191.252.60.107:3000', // seu frontend
-        methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-        allowedHeaders: ['Content-Type','Authorization'],
-    };
-
-    app.use(cors(corsOptions));
+    app.use(cors());
 
     app.use(express.static('public', { extensions: ['html'] }));
     app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
@@ -29,11 +22,6 @@
     app.use('/events', eventRoutes); 
     app.use('/action', actionRoutes); 
 
-    const sslOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/maisecultura.com.br/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/maisecultura.com.br/fullchain.pem')
-};
-
-https.createServer(sslOptions, app).listen(process.env.PORT, '0.0.0.0', () => {
-    console.log(`Backend rodando em https://localhost:${process.env.PORT}`);
-});
+    app.listen(process.env.PORT, '0.0.0.0', () => {
+        console.log(`Server rodando em http://localhost:${process.env.PORT}`)
+    })
